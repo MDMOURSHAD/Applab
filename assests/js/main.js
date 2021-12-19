e9a8d10ac4f1d8743be7ddccfd3183725d4e7778
Md Mourshad <77190784+MDMOURSHAD@users.noreply.github.com>
2021-12-19 21:20:56 +0600

    Add files via upload

--- assests/js/main.js
+++ assests/js/main.js
@@ -72,6 +72,30 @@ var swiper1 = new Swiper('.swiper1', {
   }
 });
 
+// Magnific popup video //
+$('.video').magnificPopup({
+            type: 'iframe',
+            // other options
+            iframe: {
+                markup: '<div class="mfp-iframe-scaler">' +
+                    '<div class="mfp-close"></div>' +
+                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                    '</div>',
+
+                patterns: {
+                    youtube: {
+                        index: 'youtube.com/',
+
+                        id: 'v=',
+
+                        src: 'https://www.youtube.com/embed/%id%'
+                    },
+                },
+
+             srcAction: 'iframe_src', 
+            }
+  });
+
 // Testimonial-section Slider //
 
 var swiper2 = new Swiper('.swiper2', {
